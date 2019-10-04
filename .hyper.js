3e71a80b399611912b7de3eae2e4701590e66208
Caleb Eby <caleb.eby01@gmail.com>
2019-10-04 10:52:58 -0700

    Add hyper config

---
+++ .hyper.js
@@ -0,0 +1,13 @@
+module.exports = {
+  config: {
+    fontFamily: '"Dank Mono"',
+    fontSize: 11,
+  },
+  plugins: [
+    //Themes
+    // 'hyper-gruv',
+    // 'hyper-city-lights',
+    'hyper-flat',
+    'hyperterm-paste',
+  ],
+}
