86cafbb410a1306657c5c3f8c45e4839814d795d
Caleb Eby <caleb.eby01@gmail.com>
2019-11-27 10:23:28 -0800

    Ignore dist folder for folder previews

--- fish/functions/j.fish
+++ fish/functions/j.fish
@@ -1,5 +1,5 @@
 function j
-  set -l dir (ls -d ~/Programming/src/*/*/* | sed 's/\/home\/caleb\/Programming\/src\///' | fzf --preview "tree -C $HOME/Programming/src/{} -I 'node_modules|__pycache__'")
+  set -l dir (ls -d ~/Programming/src/*/*/* | sed 's/\/home\/caleb\/Programming\/src\///' | fzf --preview "tree -C $HOME/Programming/src/{} -I 'node_modules|__pycache__|dist'")
   cd "$HOME/Programming/src/$dir"
   clear
 end
