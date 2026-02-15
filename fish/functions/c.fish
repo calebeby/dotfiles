c10130aba00c33a3f9197b7890d0b700a00b3817
Caleb Eby <caleb.eby01@gmail.com>
2026-02-15 15:23:33 -0800

    Add c shell shortcut to open nvim in curriculum folder

--- fish/functions/c.fish
+++ fish/functions/c.fish
@@ -1,4 +1,15 @@
 function c
   cd ~/Curriculum
-  nvim +"lua vim.schedule(function () require('snacks').terminal.open('fish -C \"bilbo serve\"', {win = {position = 'bottom'}}) end)"
+  nvim +"lua vim.schedule(function ()
+    local started = false
+    require('snacks').terminal.open(null, {
+      win = {
+        on_buf = function(buf)
+            if started then return end
+            started = true
+            vim.api.nvim_feedkeys('bilbo serve\n', 'n', false)
+        end
+      }
+    })
+  end)"
 end
